7e42780d79592cbc1372a057c318eb4c7e45136d
Koswara <koswara.c79@gmail.com>
2024-03-26 13:59:09 +0700

    useRef useMemo

--- 04belajar-react-keempat/src/App.jsx
+++ 04belajar-react-keempat/src/App.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import UserList from './components/UserList';
-import PageNotFound from './components/PageNotFound';
-import { AuthProvider } from './context/AuthContext';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import UserList from "./components/UserList";
+import PageNotFound from "./components/PageNotFound";
+import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
+import ProductList from "./components/ProductList";
+import ShoppingCart from "./components/ShoppingCart";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    // <AuthProvider>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element={<Login />} />
+    //       <Route path="/dashboard" element={<Dashboard />} />
+    //       <Route path="/users" element={<UserList />} />
+    //       <Route path="*" element={<PageNotFound />} />
+    //     </Routes>
+    //   </Router>
+    // </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
